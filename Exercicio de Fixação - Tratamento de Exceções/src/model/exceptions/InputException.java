694e5142ff4e6d127a2e3828c65b03b6716ff540
Luciano Zimmermann <lucianolz123@gmail.com>
2023-11-27 16:45:31 -0300

    Exercicio Fixação - Tratamento de Exceções

---
+++ Exercicio de Fixação - Tratamento de Exceções/src/model/exceptions/InputException.java
@@ -0,0 +1,9 @@
+package model.exceptions;
+
+public class InputException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public InputException (String msg) {
+		super(msg);
+	}
+}
